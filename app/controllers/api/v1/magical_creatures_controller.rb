520291ef75619a828757200d694cef073def03b2
wongjenn <wongjenn@yahoo.com>
2017-05-18 21:20:22 -0400

    corrected to show api

--- app/controllers/api/v1/magical_creatures_controller.rb
+++ app/controllers/api/v1/magical_creatures_controller.rb
@@ -1,37 +1,35 @@
-module Api::V1
-  class MagicalCreaturesController < ApplicationController
-    before_action :find_id, only: [:show, :update, :destroy]
+class Api::V1::MagicalCreaturesController < ApplicationController
+  before_action :find_id, only: [:show, :update, :destroy]
 
-    def index
-      @magical_creatures = MagicalCreature.all
-      render json: @magical_creatures
-    end
-
-    def create
-      @magical_creature = MagicalCreature.create(name: params[:magical_creature][:name])
-      render json: @magical_creature
-    end
-
-    def show
-      # @magical_creature = MagicalCreature.find(params[:id])
-      render json: @magical_creature, serializer: MagicalCreatureShowSerializer
-    end
-
-    def update
-      # @magical_creature = MagicalCreature.find(params[:id])
-      @magical_creature.update(params[:name])
-    end
-
-    def destroy
-      # @magical_creature = MagicalCreature.find(params[:id])
-      @magical_creature.destroy
-    end
-
-    private
-
-    def find_id
-      @magical_creature = MagicalCreature.find(params[:id])
-    end
-
+  def index
+    @magical_creatures = MagicalCreature.all
+    render json: @magical_creatures
   end
+
+  def create
+    @magical_creature = MagicalCreature.create(name: params[:magical_creature][:name])
+    render json: @magical_creature
+  end
+
+  def show
+    # @magical_creature = MagicalCreature.find(params[:id])
+    render json: @magical_creature, serializer: MagicalCreatureShowSerializer
+  end
+
+  def update
+    # @magical_creature = MagicalCreature.find(params[:id])
+    @magical_creature.update(params[:name])
+  end
+
+  def destroy
+    # @magical_creature = MagicalCreature.find(params[:id])
+    @magical_creature.destroy
+  end
+
+  private
+
+  def find_id
+    @magical_creature = MagicalCreature.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-scope module: 'api'do
-
-  namespace :v1 do
-    resources :magical_creatures
+  namespace 'api'do
+    namespace :v1 do
+      resources :magical_creatures
+    end
   end
-end
-
-
 end
