400b442bac2b9f0a1e1c9a34604cff9745949d24
wongjenn <wongjenn@yahoo.com>
2017-07-10 21:34:57 -0400

    changed from sqlite3 to PostgreSQL

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ end
 gem 'rails', '~> 5.0.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+gem 'pg'
+gem 'taps'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 20170220024312) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "spotify_id"
+  end
 
   create_table "folklores", force: :cascade do |t|
     t.string  "reference"
@@ -23,8 +33,41 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "description"
   end
 
+  create_table "playlist_tracks", force: :cascade do |t|
+    t.integer  "playlist_id"
+    t.integer  "track_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
   create_table "regions", force: :cascade do |t|
     t.string "name"
   end
 
+  create_table "tracks", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "artist_id"
+    t.string   "spotify_track_id"
+    t.string   "spotify_album_id"
+    t.index ["artist_id"], name: "index_tracks_on_artist_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "email"
+  end
+
+  add_foreign_key "tracks", "artists"
 end
